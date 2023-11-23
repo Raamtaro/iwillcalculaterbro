3e8eace08cfd3e2a791fe5d52e1cf86b979ef060
Raam <raam.sanghani@gmail.com>
2023-11-23 12:47:02 -0500

    added rough code for operate(a,b,operator) -- My idea is that this will be called when the compute button is hit on the actual UI calculator. It'll then populate the args for operate (maybe via an event listener) based on what is currently in the display screen and perform the calculation... theoretically, lol

--- script.js
+++ script.js
@@ -6,31 +6,57 @@ const subtract = function(a, b) {
   return a - b
 };
 
-const sum = function(numberSet) {
-  return numberSet.reduce((total, iteration) => total + iteration, 0)
-};
+const multiply = function(a, b) {
+  return a * b
+}
 
-const multiply = function(numberSet) {
-  return numberSet.reduce((total, iteration) => total * iteration, 1)
-};
 
-const division = function(a, b) {
+
+
+const divide = function(a, b) {
     return a / b
 }
 
-const power = function(base, exponent) {
-  return base**exponent
-};
+function operate(a, b, operator) {
+//This is going to be VERY rough code for now
+    let result = 0;
 
-const factorial = function(base) {
-	let basedArray = [];
-  if (base === 0) return 1
-  else {
-    for (i = base; i >= 1; i--) {
-      basedArray.push(i)
-    };
-  };
-  return basedArray.reduce((product, iteration) => product * iteration, 1)
+    if (operator === 'addition') result = add(a,b)
+    else if (operator === 'subtraction') result = subtract(a,b)
+    else if (operator === 'multiplication') result = multiply(a,b)
+    else if (operator === 'division') result = divide(a,b)
 
-};
+    return result
+}
 
+
+
+
+
+
+
+
+//EXTRA FEATURES TO WORK IN A BIT LATER
+// const power = function(base, exponent) {
+//   return base**exponent
+// };
+
+// const factorial = function(base) {
+// 	let basedArray = [];
+//   if (base === 0) return 1
+//   else {
+//     for (i = base; i >= 1; i--) {
+//       basedArray.push(i)
+//     };
+//   };
+//   return basedArray.reduce((product, iteration) => product * iteration, 1)
+
+// };
+
+// const sum = function(numberSet) {
+//   return numberSet.reduce((total, iteration) => total + iteration, 0)
+// };
+
+// const multiplySet = function(numberSet) {
+//   return numberSet.reduce((total, iteration) => total * iteration, 1)
+// };
